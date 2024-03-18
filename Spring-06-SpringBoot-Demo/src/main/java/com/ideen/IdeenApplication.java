a2fc98e29a7768b6480250c0d91b7389d9a520fa
IdeenNova <ideen@cydeo.com>
2024-03-18 17:21:16 -0400

    Spring Boot Demo

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/ideen/IdeenApplication.java
@@ -0,0 +1,13 @@
+package com.ideen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeenApplication.class, args);
+    }
+
+}
---
+++ Spring-06-SpringBoot-Demo/src/test/java/com/ideen/IdeenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
